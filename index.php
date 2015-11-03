9cf92a41b5e0fbeb23ceab21c9c691131253f43c
steve <admin@mail.com>
2015-11-03 14:51:19 -0300

    I

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ use arania\Scraper;
 
 //$crawler= new Crawler("http://stackoverflow.com/questions/6366351/getting-dom-elements-by-classname");
 //$crawler= new Crawler("http://localhost/dummy.html");
-$crawler= new Crawler("http://www.amazon.com/gp/bestsellers/electronics/281052/ref=s9_ri_gw_clnk/185-2243211-2423035?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-2&pf_rd_r=1G15XTXKMS4TJMYRD5JV&pf_rd_t=36701&pf_rd_p=2091268582&pf_rd_i=desktop");
+//$crawler= new Crawler("http://www.amazon.com/gp/bestsellers/electronics/281052/ref=s9_ri_gw_clnk/185-2243211-2423035?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-2&pf_rd_r=1G15XTXKMS4TJMYRD5JV&pf_rd_t=36701&pf_rd_p=2091268582&pf_rd_i=desktop");
+$crawler= new Crawler("http://www.ebay.com/rpp/electronics-es/GEO-EG-Latest-Tech-ES");
 $scraper= new Scraper($crawler);
-$fields= array("campo"=>"zg_title",
-				"nose"=>"zg_price",
-				"enlace"=>"zg_title:link"
+$fields= array("campo"=>"rttl",
+				"nose"=>"gl-cpr2",
+				"enlace"=>"rttl:link"
 		
 );
 //echo $scraper->extractData($fields);
